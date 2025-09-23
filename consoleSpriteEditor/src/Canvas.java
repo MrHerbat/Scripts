89b9eba1e1c6dacfcee6137102e532a84450d9b1
Maciej <113918343+MrHerbat@users.noreply.github.com>
2025-09-23 15:23:39 +0200

    partially completed console sprite editor
    pixel art sprite editor made in java that runs in console

---
+++ consoleSpriteEditor/src/Canvas.java
@@ -0,0 +1,208 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Canvas
+{
+    int xSize;
+    int ySize;
+    int[][] canvas;
+    public static Color[] colors = {
+            new Color(0,0,0),
+            new Color(255,0,0),
+            new Color(0,255,0),
+            new Color(0,0,255),
+            new Color(255,255,0),
+            new Color(0,255,255),
+            new Color(255,0,255),
+            new Color(255,255,255)
+    };
+    public static String[][] consoleColors = {
+            {"\u001B[30m","\u001B[40m"},
+            {"\u001B[31m","\u001B[41m"},
+            {"\u001B[32m","\u001B[42m"},
+            {"\u001B[34m","\u001B[44m"},
+            {"\u001B[33m","\u001B[43m"},
+            {"\u001B[36m","\u001B[46m"},
+            {"\u001B[35m","\u001B[45m"},
+            {"\u001B[37m","\u001B[47m"},
+            {"\u001B[0m","\u001B[0m"}
+    };
+
+    public Canvas() {
+    }
+    public void displayCanvas(int selectedX,int selectedY){
+        System.out.flush();
+        for (int i = 0; i < canvas.length; i++) {
+            for (int j = 0; j < canvas[0].length; j++) {
+                if (i==selectedY && j == selectedX){
+                    System.out.print(">"+consoleColors[canvas[i][j]][0]+consoleColors[canvas[i][j]][1]+" "+canvas[i][j]+" "+consoleColors[8][0]+"<");
+                }else{
+                    System.out.print("["+consoleColors[canvas[i][j]][0]+consoleColors[canvas[i][j]][1]+" "+canvas[i][j]+" "+consoleColors[8][0]+"]");
+                }
+
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < xSize; i++) {
+            System.out.print("=====");
+        }
+        System.out.println();
+        System.out.println(consoleColors[8][0]+"a/w/s/d = left/up/down/right | + - increment color | - - decrement color");
+        System.out.println(consoleColors[8][0]+"x - leave without saving | f - save as .png file");
+    }
+    public void createCanvas(){
+        int choice;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1 - utwórz nowy sprite \n 2 - otwórz tworzony wcześniej \n 3 - wyjdź \n zalecane jest tworzyć sprite'y mniejszych rozmiarów");
+        boolean loop = true;
+        while(loop){
+            choice = scanner.nextInt();
+            switch (choice){
+                case 1:
+                    int x,y;
+                    System.out.print("Podaj wielkość x:  ");
+                    x = scanner.nextInt();
+                    System.out.print("Podaj wielkość y:  ");
+                    y = scanner.nextInt();
+                    loop=false;
+                    canvas = new int[x][y];
+                    editCanvas();
+                    break;
+                case 2:
+                    loop=false;
+                    editCanvas();
+                    break;
+                case 3:
+                    loop=false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    public void editCanvas(){
+        String fileName;
+        Scanner scanner = new Scanner(System.in);
+        if(canvas==null){
+            System.out.print("Podaj nazwe pliku: ");
+            fileName = scanner.next();
+            pngToCanvas(fileName);
+        }
+        char c;
+        int selX = 0, selY = 0;
+        boolean loop = true;
+        while(loop){
+            displayCanvas(selX,selY);
+            c = scanner.next().charAt(0);
+            switch (c){
+                case 'x':
+                    loop = false;
+                    break;
+                case 'a':
+                    if(selX<=0){
+                        selX=(canvas[0].length-1);
+                    }else{
+                        selX--;
+                    }
+                    break;
+                case 'd':
+                    if(selX>=canvas[0].length-1){
+                        selX=0;
+                    }else{
+                        selX++;
+                    }
+                    break;
+                case 's':
+                    if(selY>=canvas.length-1){
+                        selY=0;
+                    }else{
+                        selY++;
+                    }
+                    break;
+                case 'w':
+                    if(selY<=0){
+                        selY=canvas.length-1;
+                    }else{
+                        selY--;
+                    }
+                    break;
+                case '+':
+                    if(canvas[selY][selX]==7){
+                        canvas[selY][selX]=0;
+                    }else{
+                        canvas[selY][selX]++;
+                    }
+                    break;
+                case '-':
+                    if(canvas[selY][selX]==0){
+                        canvas[selY][selX]=7;
+                    }else{
+                        canvas[selY][selX]--;
+                    }
+                    break;
+                case 'f':
+                    System.out.print("Podaj nazwe pliku: ");
+                    fileName = scanner.next();
+                    canvasToPng(canvas,fileName);
+                    break;
+                case 'o':
+                    System.out.print("Podaj nazwe pliku: ");
+                    fileName = scanner.next();
+                    pngToCanvas(fileName);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+
+    public void canvasToPng(int[][] tab, String name){
+        String path = "D:\\"+name+".png";
+        BufferedImage image = new BufferedImage(tab.length,tab[0].length, BufferedImage.TYPE_INT_RGB);
+        for (int y = 0; y < tab.length; y++) {
+            for (int x = 0; x < tab[y].length; x++) {
+                image.setRGB(x,y,colors[tab[y][x]].getRGB());
+            }
+        }
+        File ImageFile = new File(path);
+        try {
+            ImageIO.write(image, "png", ImageFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void pngToCanvas(String name){
+        String path = "D:\\"+name+".png";
+        File ImageFile = new File(path);
+
+        try{
+            var tempImage = ImageIO.read(ImageFile);
+            if(canvas == null || (canvas.length< tempImage.getHeight() && canvas[0].length<tempImage.getWidth())){
+                System.out.println(tempImage.getHeight());
+                System.out.println(tempImage.getWidth());
+                canvas = new int[tempImage.getHeight()][tempImage.getWidth()];
+            }
+            for (int i = 0; i < tempImage.getHeight(); i++) {
+                for (int j = 0; j < tempImage.getWidth(); j++) {
+                    int cId = 0;
+                    for (Color c : colors){
+                        if(c.getRGB()==tempImage.getRGB(i,j)){
+                            canvas[i][j]=cId;
+                            break;
+                        }
+                        cId++;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
---
+++ consoleSpriteEditor/src/Main.java
@@ -0,0 +1,24 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+//        int[][] tab = drawTable(8,8);
+//        tableToPNG(tab,"apple");
+        Canvas canvas = new Canvas();
+        canvas.createCanvas();
+        canvas.editCanvas();
+
+    }
+
+}
